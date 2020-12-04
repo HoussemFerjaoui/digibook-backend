2cda7b5396c2958d404442f6f5d1b4a9656fafee
Nedptr <houssem.ferjaoui@esprit.tn>
2020-12-04 11:17:44 +0100

    fixed booksearch, changed testschema toUser so the crud applies

--- routes/booksearch.js
+++ routes/booksearch.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const booksearch = express.Router();
-
+const request = require('request'); // HOW DID THIS LINE GOT REMOVED
+const verifyToken = require('../functions/verifyToken');
 
 
 
@@ -10,7 +11,7 @@ booksearch.get('/', (req,res) => {
 
 
 // TODO: is this async ?
-booksearch.get('/search', (req,res,next) => {
+booksearch.get('/search', (req,res) => {
 // lets suppose we have a booksearch(text) function
     // maybe adding var inside of the url string can be better?
     let ch = "https://www.googleapis.com/books/v1/volumes?q='"+req.body.text+"'&printType=books&langRestrict=en&orderBy=relevance&maxResults=1&key=AIzaSyDbM6KY3e8LOvB5mDzI6DA1PMn2EbIFMq4"
