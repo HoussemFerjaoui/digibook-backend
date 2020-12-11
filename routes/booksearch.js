7cd642b7817fab53394e048bccb2160a7bd988dd
Nedptr <houssem.ferjaoui@esprit.tn>
2020-12-12 00:51:46 +0100

    added upload pictures to /uploads, tested upload picture > translated with tesseract , now I should make it work with good api books

--- routes/booksearch.js
+++ routes/booksearch.js
@@ -2,12 +2,67 @@ const express = require('express');
 const booksearch = express.Router();
 const request = require('request'); // HOW DID THIS LINE GOT REMOVED
 const verifyToken = require('../functions/verifyToken');
+const Tesseract = require('tesseract.js');
+
+
+var multer  = require('multer');
+const path = require('path');
+//const { path } = require('dotenv/lib/env-options');
+
+// Set Multer Storage Engine
+const storage = multer.diskStorage({
+    destination: 'uploads/',
+    filename: function(req, file, cb) {
+        cb(null, Date.now() + '-' + file.originalname);
+    }
+});
+
+// Init multer upload
+const upload = multer({
+    storage: storage
+}).single('textimage');
 
 
 
 booksearch.get('/', (req,res) => {
     res.end("Welcome to book search route!");
 });
+
+// NOT FINISHED 
+/*
+booksearch.post('/up', (req,res, next) => {
+    upload(req, res, (err) => {
+        if(err) {
+            res.send(err);
+        } else {
+            //res.send(req.file);
+            Tesseract.recognize('uploads/' + req.file.filename, 'eng', { logger: m => console.log(m) })
+                .then(({ data: { text } }) => {
+                    console.log('success! Text: \n' + text);
+                    console.log(req.file);
+                    res.send(text)
+                    passsing converted text into google books api
+                    let ch = "https://www.googleapis.com/books/v1/volumes?q=\""+text.toString()+"\"&printType=books&langRestrict=en&orderBy=relevance&maxResults=1&key=AIzaSyDbM6KY3e8LOvB5mDzI6DA1PMn2EbIFMq4"
+                    console.log(ch + '\n');
+                    const options = {
+                        url: ch,
+                        method: 'GET'
+                    };
+                    request(options, function(err, gres, body) {
+                        let json = JSON.parse(body);
+                        res.send(json);
+                        console.log(json.items[0].volumeInfo.title);
+                    }); 
+
+                    next();
+                  }) 
+                .catch(error => {
+                    console.log(error.message)
+                    next();
+                })
+        }
+    })
+});*/
 
 
 // TODO: is this async ?
